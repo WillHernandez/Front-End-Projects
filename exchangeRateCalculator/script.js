6fa320309291ec2194f17f057fb7aead87151c2f
Will Hernandez <w.hernandez.code@gmail.com>
2022-03-03 10:31:50 -0500

    updated files after API changes

--- exchangeRateCalculator/script.js
+++ exchangeRateCalculator/script.js
@@ -1,42 +1,26 @@
-// using JSON.fetch to get exchange rate api
-
 // could not find a free exchange api that would allow the base code to be changed so USD is the online option at the moment
 const selectOne = document.getElementById('currency-one');
 const amountOne = document.getElementById('amount-one');
-const swap = document.getElementById('btn');
-const rateText = document.getElementById('rate');
 const selectTwo = document.getElementById('currency-two');
 const amountTwo = document.getElementById('amount-two');
 
-// function calculate will be called on every element
-// Fetches exchange rates and updates the dom
+const calculate = async () => {
+	const url = `http://api.exchangeratesapi.io/v1/latest?access_key=9393704c9c12219feeefa852cf710538&symbols=USD,AUD,CAD,PLN,MXN&format=1`
 
-function calculate(){
-
-  const countCode1 = selectOne.value;
-  const countCode2 = selectTwo.value;
-
-  fetch(`https://api.exchangeratesapi.io/latest?base=${countCode1}`)
-    .then(res => res.json())
-    .then(data => {
-      let rate = data.rates[countCode2];
-      rateText.innerText = (`1 ${countCode1} = ${rate} ${countCode2}`);
-      amountTwo.value = (amountOne.value * rate).toFixed(2);
-    })
-};
+	try {
+		const res = await fetch(url);
+		const data = await res.json();
+		let secondRate = data.rates[selectTwo.value];
+		amountTwo.value = String((Number(amountOne.value) * Number(secondRate)).toFixed(2));
+	} catch(err) {
+		console.log(err);
+	}
+}
 
 selectOne.addEventListener('change', calculate);    
 amountOne.addEventListener('input', calculate);
 selectTwo.addEventListener('change', calculate);
 amountTwo.addEventListener('input', calculate);
-
-swap.addEventListener('click', () => {
-  debugger;
-  let temp = selectOne.value;
-  selectOne.value = selectTwo.value;
-  selectTwo.value = temp;
-  calculate();
-})
 
 calculate();
 
